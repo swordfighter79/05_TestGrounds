9cd0b7da6c2bc1270993ed71c681ae48dd00bd66
Robert <swordfighter79@gmail.com>
2018-11-20 14:58:53 -0500

    Testing Grounds Lecture 244  Converting Blueprints to C++

--- Source/S05_TestingGrounds/ChooseNextWaypoint.cpp
+++ Source/S05_TestingGrounds/ChooseNextWaypoint.cpp
@@ -1,16 +1,37 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "ChooseNextWaypoint.h"
+#include "AIController.h"
+#include "PatrollingGuard.h"                     //  TODO remove coupling
 #include "BehaviorTree/BlackboardComponent.h"
+
 
 
 
 
 EBTNodeResult::Type UChooseNextWaypoint::ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory)
 {
+
+	// TODO Protect against empty patrol routes
+
+	// Get Patrol Points
+	auto AIController = OwnerComp.GetAIOwner();
+	auto ControlledPawn = AIController->GetPawn();
+	auto PatrollingGuard = Cast< APatrollingGuard>(ControlledPawn);
+	auto PatrolPoints = PatrollingGuard->PatrolPointsCPP;
+
+	// Set next waypoint
 	auto BlackboardComp = OwnerComp.GetBlackboardComponent();
 	auto Index = BlackboardComp->GetValueAsInt(IndexKey.SelectedKeyName);
-	UE_LOG(LogTemp, Warning, TEXT("Waypoint Index: %i"), Index);
+	BlackboardComp->SetValueAsObject(WaypointKey.SelectedKeyName, PatrolPoints[Index]);
+
+	// Cycle the index
+	auto NextIndex = (Index + 1) % PatrolPoints.Num();
+	BlackboardComp->SetValueAsInt(IndexKey.SelectedKeyName, NextIndex);
+
+
+
+	//UE_LOG(LogTemp, Warning, TEXT("Waypoint Index: %i"), Index);
 
 	return EBTNodeResult::Succeeded;
 	
--- Source/S05_TestingGrounds/PatrollingGuard.h
+++ Source/S05_TestingGrounds/PatrollingGuard.h
@@ -14,6 +14,7 @@ class S05_TESTINGGROUNDS_API APatrollingGuard : public ATP_ThirdPersonCharacter
 {
 	GENERATED_BODY()
 
+public:    // TODO remove or create Get
 		UPROPERTY (EditInstanceOnly, Category = "Patrol Route")
 		TArray<AActor*> PatrolPointsCPP;
 	
