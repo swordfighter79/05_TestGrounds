bde57cdca78112545c8f3be11679c970351b5514
Robert <swordfighter79@gmail.com>
2018-11-20 13:11:52 -0500

    Testing Grounds Lecture 242  Reading Blackboard Data in C++

--- Source/S05_TestingGrounds/ChooseNextWaypoint.h
+++ Source/S05_TestingGrounds/ChooseNextWaypoint.h
@@ -16,5 +16,7 @@ class S05_TESTINGGROUNDS_API UChooseNextWaypoint : public UBTTaskNode
 	
 	virtual EBTNodeResult::Type ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory) override;
 	
-	
+protected:
+	UPROPERTY(EditAnywhere, Category = "blackboard")
+		struct FBlackboardKeySelector IndexKey;
 };
