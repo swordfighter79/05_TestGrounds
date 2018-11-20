278986832ac6544deed7f17cd1c6f56ea4e587ca
Robert <swordfighter79@gmail.com>
2018-11-20 12:35:01 -0500

    Testing Grounds Lecture 241  C++ behaviour tree task

---
+++ Source/S05_TestingGrounds/ChooseNextWaypoint.cpp
@@ -0,0 +1,14 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "ChooseNextWaypoint.h"
+
+
+
+
+
+EBTNodeResult::Type UChooseNextWaypoint::ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory)
+{
+	UE_LOG(LogTemp, Warning, TEXT("AI in C++ - Yay!"));
+	return EBTNodeResult::Succeeded;
+	
+}
